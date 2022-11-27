d99d1e722963e34af3d0731fbea63726320a6835
wb <wb@bridge.ishiganto.net>
2022-11-27 15:45:06 +0000

    Added SendGrid email support

--- bgpq3-auto.sh
+++ bgpq3-auto.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source .env
+
 LOG_FILE="$(pwd)/checks.log"
 
 add_log()
@@ -8,6 +10,14 @@ add_log()
 	echo "${LOG_DT} [${2}] ${1}" >> $LOG_FILE
 }
 
+send_email()
+{
+	ENCODED=${2//$'\n'/<br/>} # Replace \n with <br/> for email formatting
+	ENCODED=$(echo ${ENCODED} | sed -e 's/ /\&nbsp;/g') # Replace spaces with &nbsp for email formatting
+	 
+	# Use SendGrid for emailing updates
+	curl --request POST --url "https://api.sendgrid.com/v3/mail/send" --header "Authorization: Bearer ${SENDGRID_API_KEY}" --header 'Content-Type: application/json' --data '{"personalizations": [{"to": [{"email": "'${EMAIL_TO}'"}]}],"from": {"email": "'${EMAIL_FROM}'"},"subject":"[bgpq3] Peer Update","content": [{"type": "text/html","value": "'${1}' has an updated prefix list.<p>'${ENCODED}'</p>"}]}'
+}
 
 if [ -z $1 ]
 then
@@ -37,7 +47,7 @@ do
 	bgpq3 -S ARIN,ALTDB,RADB,LEVEL3,NTTCOM,RIPE -AXR 24 -m 24 -l $PEER_UC-IN-IPV4 $AS_SET >> "${TIME}.tmp"
 	
 	# For Dev Purposes, add timestamp to end of file so there's a difference
-	# echo ${TIME} >> "${TIME}.tmp"
+	 echo ${TIME} >> "${TIME}.tmp"
 
 	# Get a list of .cur files
 	CUR_ARR=(`find ./ -maxdepth 1 -name "*.cur"`)
@@ -58,12 +68,14 @@ do
 			find ./ -maxdepth 1 -type f -name "*.last" -delete
 			mv $FILE "${FILENAME}.last"
 			mv "${TIME}.tmp" "${TIME}.cur"
-			add_log "Files moved" ${peer}
-		fi
 
+			add_log "Files moved" ${peer}
+			send_email "${peer}" "`cat ${TIME}.cur`"
+		fi
 	else
 		mv "${TIME}.tmp" "${TIME}.cur"
-		add_log "Initial query run." ${peer}
+		add_log "Initial query completed" ${peer}
+		send_email "${peer}" "`cat ${TIME}.cur`"
 	fi
 	cd ..
 	add_log "Query completed" ${peer}
